7426830c5a1cec123e412ef5b46cc76ca516e228
Andriy Novak <kruzerandriy@gmail.com>
2015-06-04 00:53:32 +0300

    add web client

---
+++ Novak.Andriy/network_progekts/WebChatClient/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Windows;
+
+namespace WebChatClient
+{
+    public partial class MainWindow : Window
+    {
+        private readonly WebClient _webClient;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _webClient = new WebClient(IPAddress.Loopback.ToString(),4567);
+        }
+
+        private void BSend_OnClick(object sender, RoutedEventArgs e)
+        {
+            _webClient.SendMessage(tbText.Text);
+
+        }
+    }
+}
---
+++ Novak.Andriy/network_progekts/WebChatClient/WebClient.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace WebChatClient
+{
+    public class WebClient
+    {
+        private const int BuferSize = 256;
+        private TcpClient _client;
+       
+
+        public WebClient(string host, int port)
+        {
+            _client = new TcpClient();
+            _client.ConnectAsync(IPAddress.Loopback, port);
+            
+        }
+
+        public void SendMessage(string message)
+        {
+            var stream = _client.GetStream();
+            var messageBytes = Encoding.ASCII.GetBytes(message);
+            stream.WriteAsync(messageBytes, 0, messageBytes.Length);
+            stream.Flush();
+            stream.Dispose();
+        }
+
+        //public string GetMessage()
+        //{
+        //    var buffer = new byte[BuferSize];
+           
+        //    stream.ReadAsync(buffer, 0, buffer.Length - 1);
+        //    stream.Flush();
+        //    stream.Close();
+        //    return Encoding.ASCII.GetString(buffer);
+        //}
+
+        public void WebClientStop()
+        {
+            _client.Close();    
+        }
+    }
+}
