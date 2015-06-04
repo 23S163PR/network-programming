c278085121275531c8954f9d089b40f8d9696104
Valentin <pizhik.valentin@gmail.com>
2015-06-04 12:46:15 +0300

    Init
    add my folder in network-programming, and initialize project message
    service

---
+++ Pyzhyk.Valentyn/Message service (Console variant)/Message service (Console variant)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Message_service__Console_variant_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
