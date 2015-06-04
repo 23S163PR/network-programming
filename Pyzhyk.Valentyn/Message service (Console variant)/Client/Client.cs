a346989e5d95efea5bc83a1d0e04ea57437af596
Valentin <pizhik.valentin@gmail.com>
2015-06-04 14:37:49 +0300

    init class Server and Client

---
+++ Pyzhyk.Valentyn/Message service (Console variant)/Client/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Client
+    {
+        private int Port;
+        private IPAddress IpAddress;
+        private TcpClient client;
+
+        public Client()
+        {
+            Port = 2335;
+            IpAddress = IPAddress.Loopback;
+            client = new TcpClient();
+        }
+        public void SendMessagesToServer(string message)
+        {
+            client.Connect(IpAddress, Port);
+            var stream = client.GetStream();
+            var messageBytes = Encoding.ASCII.GetBytes(message);
+            Console.WriteLine("Entered message: {0}", message);
+            stream.Write(messageBytes, 0, messageBytes.Length);
+            stream.Flush();
+            client.Close();
+        }
+
+    }
+}
---
+++ Pyzhyk.Valentyn/Message service (Console variant)/Server/Server.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Server
+    {
+        private int Port;
+        private int MaxClientCount;
+        private const int MaxMessageSizeInBytes = 1024;
+
+        public void StartListenAllIPAdressInPort(int port, int maxClientCount)
+        {
+            Port = port;
+            MaxClientCount = maxClientCount;
+            var listener = new TcpListener(IPAddress.Any, Port);
+            listener.Start(MaxClientCount);
+
+            var buffer = new byte[MaxMessageSizeInBytes];
+            var socket = listener.AcceptSocket();
+            while (true)
+            {   
+
+                var received = socket.Receive(buffer);
+
+                if (received > 0)
+                {
+                    var receivedMessage = Encoding.ASCII.GetString(buffer, 0, received);
+                    Console.WriteLine("CLIENT:: {0}", receivedMessage);
+                }
+            }
+        }
+    }
+}
