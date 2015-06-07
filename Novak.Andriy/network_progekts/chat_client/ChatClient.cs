e9c19abeaa3cd7f93fddc016ddc71b71c9e8156f
Andriy Novak <kruzerandriy@gmail.com>
2015-06-07 18:45:46 +0300

    New chat client with opportynity send json objects

---
+++ Novak.Andriy/network_progekts/chat_client/ChatClient.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using ServerJsonObject;
+
+
+namespace chat_client
+{
+    public class ChatClient 
+    {
+        private TcpClient _tcpСlient;
+        private NetworkStream _networkStream;
+        private bool _stopNetwork;
+        private readonly StringBuilder _message;
+        private string _username;
+        public bool DataAviable { get { return _networkStream.DataAvailable; } }
+
+        public ChatClient(string username)
+        {
+            _username = username;
+            _message = new StringBuilder();
+        }
+
+        public void Connect(IPAddress adress, int port)
+        {
+            try
+            {
+                _tcpСlient = new TcpClient();
+                _tcpСlient.Connect(adress, port);
+                _networkStream = _tcpСlient.GetStream();
+            }
+            catch
+            {
+            }
+        }
+
+        public void CloseClient()
+        {
+            if (_networkStream != null) _networkStream.Close();
+            if (_tcpСlient != null) _tcpСlient.Close();
+            _stopNetwork = true;
+        }
+
+        public void SendMessage(string msg, int code = 1)
+        {
+           if(_networkStream == null)return;
+
+           var buffer = Encoding.ASCII.GetBytes(new ChatObject(_username, "", code, msg).ObjectToJson());
+            _networkStream.WriteAsync(buffer, 0, buffer.Length);  
+        }
+
+        public string ReceiveRun()
+        {
+            _message.Clear();
+            if (!_tcpСlient.Connected || _stopNetwork) return null;
+
+            try
+            {
+                while (_tcpСlient.Available > 0)
+                {
+                    var buffer = new byte[_tcpСlient.Available]; // create buffer for data
+                    _networkStream.ReadAsync(buffer, 0, buffer.Length);
+                    _networkStream.FlushAsync();
+                    _message.Append(Encoding.ASCII.GetString(buffer));
+                }
+                return _message.Length <= 0 ? null : _message.ToString();
+            }
+            catch
+            {
+                
+            }
+            return null;
+        }       
+    }
+}
---
+++ Novak.Andriy/network_progekts/chat_client/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using ServerJsonObject;
+
+namespace chat_client
+{
+    public partial class MainWindow : Window
+    {
+        private ChatClient _client;
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _client = new ChatClient("user");
+          
+            Closed += (sender, args) =>
+            {
+                _client.SendMessage("404", 404/*close client code for server*/);
+                
+                _client.CloseClient();
+            };
+        }
+
+       
+        private void BSend_OnClick(object sender, RoutedEventArgs e)
+        {
+            if(!tbText.Text.Any())return;
+            _client.SendMessage(tbText.Text);
+        }
+
+        private void ConectClick(object sender, RoutedEventArgs e)
+        {
+            _client.Connect(IPAddress.Loopback, 4567);
+            var thread = new Thread(ReciveMessage);
+            thread.Start();
+        }
+
+        private void ReciveMessage()
+        {
+            while (true)
+            {
+                if (!_client.DataAviable) continue;
+                var data = _client.ReceiveRun().JsonToObject();
+                var formatedData = string.Format("\n>>{0}\n{1}\n{2}", data.Login, data.Message, DateTime.Now);
+                tbBox.CheckAppendText(formatedData);
+            }
+        }
+    }
+
+    public static class TextBoxExtensions
+    {
+        public static void CheckAppendText(this TextBoxBase textBox, string msg, bool waitUntilReturn = false)
+        {
+            Action append = () => textBox.AppendText(msg);
+            if (textBox.CheckAccess())
+            {
+                append();
+            }
+            else if (waitUntilReturn)
+            {
+                textBox.Dispatcher.Invoke(append);
+            }
+            else
+            {
+                textBox.Dispatcher.BeginInvoke(append);
+            }
+        }
+    }
+}
