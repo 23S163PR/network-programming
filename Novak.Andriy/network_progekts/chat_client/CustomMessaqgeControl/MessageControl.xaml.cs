946d656b2160046a842efcb6b8cfcbe9119d94a6
Andriy Novak <kruzerandriy@gmail.com>
2015-06-09 23:25:32 +0300

    add new message style

--- Novak.Andriy/network_progekts/chat_client/CustomMessaqgeControl/MessageControl.xaml.cs
+++ Novak.Andriy/network_progekts/chat_client/CustomMessaqgeControl/MessageControl.xaml.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace chat_client.CustomMessaqgeControl
 {
     public partial class MessageControl : UserControl
     {
-        public MessageControl(string login, string message)
+        public MessageControl(string login, string message, bool aligmentFlag)
         {
             InitializeComponent();
+            HorizontalAlignment = aligmentFlag ? HorizontalAlignment.Left : HorizontalAlignment.Right;
             tbMessage.Text = message;
             lbLogin.Content = login;
             lbDate.Content = DateTime.Now;
