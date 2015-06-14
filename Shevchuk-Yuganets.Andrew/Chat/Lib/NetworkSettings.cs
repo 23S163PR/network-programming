d78bff2f3167828804b3e41b75b002c9d4800d69
Andrew Shevchuk-Yuganets <mazillko@gmail.com>
2015-06-14 17:54:35 +0300

    Chat message height fix

--- Shevchuk-Yuganets.Andrew/Chat/Lib/NetworkSettings.cs
+++ Shevchuk-Yuganets.Andrew/Chat/Lib/NetworkSettings.cs
@@ -2,7 +2,7 @@
 {
 	public static class NetworkSettings
 	{
-		public static readonly int MaxMessageSizeInBytes = 2097152/8;
+		public static readonly int MaxMessageSizeInBytes = 2097152/4;
 		public static readonly int ServerPort = 1335;
 		public static readonly string ServerIp = "127.0.0.1";
 	}
