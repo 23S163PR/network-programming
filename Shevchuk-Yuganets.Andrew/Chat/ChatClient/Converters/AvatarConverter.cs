742d94956ac9127422210f0ea4016aa0de4bd89e
Andrew Shevchuk-Yuganets <mazillko@gmail.com>
2015-06-16 16:22:39 +0300

    Minor changes

--- Shevchuk-Yuganets.Andrew/Chat/ChatClient/Converters/AvatarConverter.cs
+++ Shevchuk-Yuganets.Andrew/Chat/ChatClient/Converters/AvatarConverter.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Windows.Data;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace ChatClient.Converters
