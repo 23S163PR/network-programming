323348ef7f4b67ebd8c37a6e45f32247567d68ab
Andrew Shevchuk-Yuganets <mazillko@gmail.com>
2015-06-21 22:42:10 +0300

    Added chat server service log console

---
+++ Shevchuk-Yuganets.Andrew/Chat/ChatServerServiceLogConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatServerServiceLogConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const int port = 1339;
+			var sender = new IPEndPoint(IPAddress.Any, 0);
+			var client = new UdpClient(port);
+
+			while (true)
+			{
+				var bytes = client.Receive(ref sender);
+				var logMessage = Encoding.ASCII.GetString(bytes);
+
+				Console.WriteLine(logMessage);
+			}
+		}
+	}
+}
